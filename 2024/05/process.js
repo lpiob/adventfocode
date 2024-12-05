b276261264a1c65a1d1a50388f99ed4a4deb3f22
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-05 08:53:05 +0100

    WIP: sorting

--- 2024/05/process.js
+++ 2024/05/process.js
@@ -12,11 +12,14 @@ const [pros, updates] = input.split('\n\n');
 
 
 let sum_mpn=0;
+let sum_corrected_mpn=0;
+
 for (const update of updates.split('\n')) {
   const [check, mpn] = updateAdheresToPros(update, pros);
-  if (check) {
+  if (check) { // correctly_ordered_updated
     console.log(update, check, mpn);
     sum_mpn+=mpn;
+  } else { // incorrectly_ordered_update
   }
 }
 
@@ -24,16 +27,47 @@ console.log("Sum for MPN: ", sum_mpn);
 
 function updateAdheresToPros(update, pros) {
   const pages=update.split(',');
-  console.log(pages);
 
   for (pro of pros.split('\n')) {
     const [n1, n2] = pro.split('|');
     const [in1, in2] = [pages.indexOf(n1), pages.indexOf(n2)];
 
-    if (in1>=0 && in2>=0 && in2<=in1)
+    if (in1>=0 && in2>=0 && in2<=in1) {
+      fixOrdering(update);
       return [false, -1];
+    }
 
   }
   const mpn=Number(pages[Math.floor(pages.length/2)]);
   return [true, mpn];
+}
+
+function fixOrdering(update) {
+  const pages=update.split(',');
+  console.log('before sort', pages);
+  pages.sort(sortWithPros);
+  console.log('after sort', pages);
+}
+
+function sortWithPros(a,b) {
+  a=Number(a);
+  b=Number(b);
+  for (pro of pros.split('\n')) {
+    let [n1, n2] = pro.split('|');
+    n1=Number(n1);
+    n2=Number(n2);
+
+    if (n1==a && n2==b) {
+      console.log("v1",a,b, a>b ? -1 : 1);
+      return a>b ? -1 : 1;
+    } else if (n1==b && n2==a) {
+      console.log("v2",a,b, a>b ? -1 : 1);
+      return a>b ? -1 : 1;
+    } else {
+      //console.log("v3");
+      //return 0;
+    };
+
+  }
+  return 0;
 }
