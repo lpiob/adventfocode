97cc10f5855ec221210f71837f75c14232e4ae0b
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-06 10:08:26 +0100

    WIP: hard time crashing

--- 2024/06/process_part2.js
+++ 2024/06/process_part2.js
@@ -63,46 +63,84 @@ function calculatenewposition(y,x, guard_direction) {
   return [y,x]
 }
 
-function makeamove(){
-  console.log("We are at ", y, x, " direction: ", guard_direction);
+function makeamove(y,x,guard_direction,maze_obstacles,maze_visited,maze_visited_dir){
+  //console.log("We are at ", y, x, " direction: ", guard_direction);
   let [ny,nx]=calculatenewposition(y,x,guard_direction);
-  console.log("New position would be ", ny, nx);
+  //console.log("New position would be ", ny, nx);
   if (ny<0 || ny>=maze_height || nx<0 || nx>=maze_width) {
-    console.log("Guard will leave the maze");
-    console.log("He has visited ", countdistinctvisitedpositions(maze_visited), "distinct cells");
+    //console.log("Guard will leave the maze");
+    //console.log("He has visited ", countdistinctvisitedpositions(maze_visited), "distinct cells");
     throw new Error("oob");
   } else if (maze_visited_dir[guard_direction][ny][nx]>0) {
     console.log("Guard has revisited his previous position");
     console.log("He is stuck in a time loop");
     throw new Error("timeloop");
   } else if (maze_obstacles[ny][nx]=="#") {
-    console.log("Obstacle ahead. NOT making a move, but turning right");
+    //console.log("Obstacle ahead. NOT making a move, but turning right");
     guard_direction=(guard_direction+2)%8
     maze_visited_dir[guard_direction][y][x]+=1;   
-    return makeamove();
+    return makeamove(y,x,guard_direction,maze_obstacles,maze_visited,maze_visited_dir);
   } else { // no obstacles, let's just make another
-    console.log("Deciding to make another move");
+    //console.log("Deciding to make another move");
     y=ny
     x=nx
     maze_visited[y][x]+=1;
     maze_visited_dir[guard_direction][y][x]+=1;
 
-    return makeamove();
+    return makeamove(y,x,guard_direction,maze_obstacles,maze_visited,maze_visited_dir);
   }
 }
 
+console.log("1st pass");
 // 1st pass - validate the maze is passable
-// no error handling on purpose
-makeamove();
-
-// 2nd pass - introduce an obstacle and check if it cause a timeloop
-maze_obstacles[6][3]="#";
-// reset guard position
-x=gsx
-y=gsy
-
 try {
-  makeamove();
+  // passing a deep copy
+  makeamove(y,x,guard_direction,maze_obstacles,
+    JSON.parse(JSON.stringify(maze_visited)),
+    JSON.parse(JSON.stringify(maze_visited_dir)));
 } catch (error) {
   console.log(error.message);
 }
+
+// brute force approach
+let timeloops=0;
+for (let ny=0; ny<maze_height; ny++) {
+  for (let nx=0; nx<maze_width; nx++) {
+    if ((nx!=gsx && ny!=gsy) && maze_obstacles[ny][nx]!='#') {
+      let new_maze_obstacles=JSON.parse(JSON.stringify(maze_obstacles));
+      new_maze_obstacles[ny][nx]='#';
+
+      //console.log("Running a sim with new obstacle at ", ny, nx);
+
+      try {
+        // passing a deep copy
+        makeamove(y,x,guard_direction,new_maze_obstacles,
+          JSON.parse(JSON.stringify(maze_visited)),
+          JSON.parse(JSON.stringify(maze_visited_dir)));
+      } catch (error) {
+        if (error.message=='timeloop') { 
+          timeloops++;
+          console.log("Time loop with obstacle at ", ny, nx);
+        }
+      }
+
+
+    }
+    
+  }
+
+}
+
+console.log("Timeloops encountered", timeloops);
+
+/*
+console.log("2nd pass");
+// 2nd pass - introduce an obstacle and check if it cause a timeloop
+maze_obstacles[6][3]="#";
+
+try {
+  makeamove(y,x,guard_direction,maze_obstacles,[...maze_visited],[...maze_visited_dir]);
+} catch (error) {
+  console.log(error.message);
+}
+*/
