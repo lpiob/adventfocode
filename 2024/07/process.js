ebdd9daa9a31f0b3b788457cca2d7bd055e99cf1
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-07 10:35:44 +0100

    Step 2

--- 2024/07/process.js
+++ 2024/07/process.js
@@ -19,7 +19,7 @@ function equationIsPossible(numbers, result) {
   let variations=[]; 
 
   const possible_insertions=numbers.length-1;
-  const operators=['+', '*']
+  const operators=['+', '*', '||']
   const totalVariations = Math.pow(operators.length, possible_insertions);
 
   for (let i=0; i < totalVariations; i++) {
@@ -29,28 +29,26 @@ function equationIsPossible(numbers, result) {
     while (ni < numbers.length) {
       variation.push(numbers[ni]);
       if (ni>0) { //(ni+1)%2==0 || ni+1==numbers.length) {
-        variation.push(operators[(Math.floor(i / Math.pow(2, oc)) % 2)]);
+        variation.push(operators[(Math.floor(i / Math.pow(3, oc)) % operators.length)]);
         oc++;
       };
       ni++;
     }
-    
-
+  
     variations.push(variation);
 
     // actually, we can check it right here
-    let evres=evaluateRPN(variation);
+    let evres=evaluateREN(variation);
     if (evres==result) { 
       return true;
     }
 
   }
 
-
   return false;
 }
 
-function evaluateRPN(tokens) {
+function evaluateREN(tokens) {
     const stack = [];
     
     for (const token of tokens) {
@@ -64,17 +62,11 @@ function evaluateRPN(tokens) {
                 case '+':
                     stack.push(a + b);
                     break;
-                case '-':
-                    stack.push(a - b);
-                    break;
                 case '*':
                     stack.push(a * b);
                     break;
-                case '/':
-                    stack.push(a / b);
-                    break;
-                case '^':
-                    stack.push(Math.pow(a, b));
+                case '||':
+                    stack.push(Number(String(a)+String(b)));
                     break;
                 default:
                     throw new Error(`Unknown operator: ${token}`);
