5682cf2c66ae2cac24477748cd2154ca5ca07988
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-08 08:11:49 +0100

    A version that works well for examples, but not for the input data

--- 2024/08/process.js
+++ 2024/08/process.js
@@ -12,7 +12,7 @@ for (let row of input.split('\n')) {
   for (let cell of row) {
     antenna_map[y][x]=cell;
     antinode_map[y][x]=".";
-    if (cell!="." && cell!="#") {
+    if (cell.match(/[a-zA-Z0-9]/)) {
       if (!antennas[cell]) antennas[cell]=[];
       antennas[cell].push([y,x]);
     }
@@ -21,7 +21,12 @@ for (let row of input.split('\n')) {
   y++; x=0;
 }
 
+const map_height=antenna_map.length
+const map_width=antenna_map[0].length;
+
 console.log(antennas);
+
+let antinodes_count=0;
 
 for (const freq in antennas) {
   console.log("Calculating antinodes for", freq)
@@ -35,16 +40,24 @@ for (const freq in antennas) {
         const dx=a[1]-b[1]
         const ny=a[0]+dy;
         const nx=a[1]+dx;
-        
-        console.log("Antinode for ",a,"and",b ," at ", ny, "x", nx)
 
-        antinode_map[ny][nx]="#"
+        if (ny>=0 && ny<map_height && nx>=0 && nx<=map_width) {
+        
+          console.log("Antinode for ",a,"and",b ," at ", ny, "x", nx)
+
+          if (antinode_map[ny][nx]!="#") {
+            antinode_map[ny][nx]="#"
+            antinodes_count++;
+          }
+        }
 
       }
     });
   });  
   //console.log(pairs);
 }
+
+console.log("Unique antinodes count: ", antinodes_count)
 
 
 function hasPair(pairs, pair) { // because .includes/.indexOf does not work on lists
