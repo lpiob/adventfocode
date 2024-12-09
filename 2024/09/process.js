6e5c335c07835c594368d4a1578ab895cc6625ba
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-09 09:23:01 +0100

    A version that works for the not-so-corner case of final input

--- 2024/09/process.js
+++ 2024/09/process.js
@@ -2,48 +2,61 @@ const input = require('fs').readFileSync(0).toString().trim();
 
 let i=0;
 
-let blocks="";
+let blocklist=[];
 
 for (r of input) {
   if (i%2==0) {
-    console.log(String(Math.floor(i/2)).repeat(r));
-    blocks=blocks+String(Math.floor(i/2)).repeat(r);
+    //console.log(String(Math.floor(i/2)).repeat(r));
+    for (let i2=0; i2<r; i2++) {
+      blocklist.push(Math.floor(i/2));
+    }
   } else {
-    console.log(".".repeat(r));
-    blocks=blocks+".".repeat(r);
+    //console.log(".".repeat(r));
+    for (let i2=0; i2<r; i2++) {
+      blocklist.push(".");
+    }
   }
   i++;
 }
 
+let blocks=blocklist.join("");
+
 console.log(blocks);
 
-let original_length=blocks.length;
+let original_length=blocklist.length;
 
 
 // moving phase
-while (blocks.match(/\./)) {
+while (blocklist.indexOf(".")>=0) {
+  last_block=blocklist.pop();
+  blocklist[blocklist.indexOf(".")]=last_block;
+  
+//  blocks=blocklist.join("");
+//  console.log(blocks);
+/*
+  last_non_free_block=blocks.match(/[^\.]\.*$/)
+  last_block=blocks.charAt(last_non_free_block.index
+  blocks=blocks.slice(0,last_non_free_block.index)+blocks.slice(last_non_free_block.index+1);
+*/
 
-  last_block=blocks.slice(-1);
-  blocks=blocks.slice(0,-1);
-
-  blocks=blocks.replace(/\./, last_block);
-  //console.log(blocks);
 
   //if (i--<=0) { console.log("unc exit");  process.exit(); };
 
 }
 
 // fill rest
-blocks=blocks+".".repeat(original_length-blocks.length);
+for (i=blocklist.length; i<original_length; i++)
+  blocklist.push(".");
+
+blocks=blocklist.join("");
+
 console.log(blocks);
 
 // calculate checksum
 let checksum=0;
-i=0;
-for (block of blocks) {
-  if (block.match(/\d/))
-    checksum+=i*Number(block);
-  i++;
+for (i=0; i<blocklist.length; i++) {
+  if (blocklist[i]!=".")
+    checksum+=i*Number(blocklist[i]);
 }
 
 console.log("Checksum", checksum);
