122599689b0594b54fc98e42d3916dc99a0383e7
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-12 22:20:53 +0100

    step2

--- 2024/12/process.js
+++ 2024/12/process.js
@@ -1,5 +1,8 @@
 const input = require('fs').readFileSync(0).toString().trim(); 
 
+// yeah, it's ugly
+// but hey, it works
+
 let map=[]; // 2d map
 let plants=[];
 
@@ -25,10 +28,14 @@ console.log(plants);
 let regions=findRegions(map);
 console.log(regions);
 let sum=0;
+let sum2=0;
 for (const reg of regions) {
   sum+=reg.perimeter*reg.size;
+  sum2+=reg.fences*reg.size;
+  console.log("Region",reg.letter,"price",reg.fences*reg.size);
+  console.log(reg.cells);
 }
-console.log(sum);
+console.log(sum,sum2);
 
 
 function findRegions(map) {
@@ -75,6 +82,82 @@ function findRegions(map) {
     
     return perimeter;
   }
+
+  function countSequences(arr) {
+    //console.log("csx", arr);
+    if (arr.length === 0) return 0;
+    if (arr.length === 1) return 1;
+    
+    let sequences = 1;
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== arr[i-1] + 1) {
+            sequences++;
+        }
+    }
+    
+    return sequences;
+  }
+
+  function pairInList(y,x,list){
+    for (el of list)
+      if (el[0]==y && el[1]==x) return true;
+    return false;
+  }
+
+  function calculateFences(letter, region) {
+    let sum=0;
+
+    // horizontal fences
+    for (let y=0; y<map_height; y++) {
+      let subtotal=0;
+
+      let cells_top=[];
+      let cells_bottom=[];
+
+      for (let x=0; x<map_width; x++) {
+        if (map[y][x]==letter && pairInList(y,x, region)) {
+          if (y==0 || (map[y-1][x]!=letter)) 
+            cells_top.push(x);
+          if (y==map_height-1 || (map[y+1][x]!=letter))
+            cells_bottom.push(x);
+        };
+      };
+      // w cells mamy liste komorek z rośliną, np. [1,2,3,8,12,13]
+      sum+=countSequences(cells_top);
+      sum+=countSequences(cells_bottom);
+      //console.log("TOP",cells_top, countSequences(cells_top));
+      //console.log('BOTTOM',cells_bottom, countSequences(cells_bottom));
+
+    }
+
+
+    // vertical fences
+    // we could just rotate the list by 90 degrees, but the lists would need to be deepcopied
+    // and it's late already ;]
+    for (let x=0; x<map_width; x++) {
+      let subtotal=0;
+
+      let cells_left=[];
+      let cells_right=[];
+
+      for (let y=0; y<map_height; y++) {
+        if (map[y][x]==letter && pairInList(y,x, region)) {
+          if (x==0 || (map[y][x-1]!=letter)) 
+            cells_left.push(y);
+          if (x==map_width-1 || (map[y][x+1]!=letter))
+            cells_right.push(y);
+        };
+      };
+      // w cells mamy liste komorek z rośliną, np. [1,2,3,8,12,13]
+      sum+=countSequences(cells_left);
+      sum+=countSequences(cells_right);
+      //console.log(cells_left, cells_right);
+    }
+
+
+    return sum;
+  }
   
   // Iterate through all cells
   for (let y = 0; y < map_height; y++) {
@@ -82,10 +165,14 @@ function findRegions(map) {
       if (!visited[y][x]) {
         const currentRegion = [];
         const perimeter = dfs(y, x, map[y][x], currentRegion);
+        console.log("cr", currentRegion)
+        console.log("fences", calculateFences(map[y][x], currentRegion));
+
         regions.push({
           letter: map[y][x],
           cells: currentRegion,
           size: currentRegion.length,
+          fences: calculateFences(map[y][x], currentRegion),
           perimeter: perimeter
         });
       }
@@ -94,3 +181,4 @@ function findRegions(map) {
   
   return regions;
 }
+
