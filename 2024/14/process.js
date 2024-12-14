b32b5b7957aefd009a25ed8dabc49ca75b237979
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-14 07:46:16 +0100

    Step2 with quality control

--- 2024/14/process.js
+++ 2024/14/process.js
@@ -31,21 +31,40 @@ for (const el of input.split('\n')) {
   // modulo with negative wrapping
   // ((position + steps) % width + width) % width;
 
-  robot.pN=[
-      ((robot.p[0] + reps*robot.v[0]) % width + width ) % width,
-      ((robot.p[1] + reps*robot.v[1]) % height + height ) % height,
-  ];
-
   robots.push(robot);
 
   };
 
 }
 
+function moveRobots(reps){
+  let lines=Array(height).fill(0);
+  let cols=Array(width).fill(0);
+  for (const robot of robots) {
+    robot.pN=[
+        ((robot.p[0] + reps*robot.v[0]) % width + width ) % width,
+        ((robot.p[1] + reps*robot.v[1]) % height + height ) % height,
+    ];
+    lines[robot.pN[1]]++;
+    cols[robot.pN[0]]++;
+  };
+  let qc=0; // quality control ;]
+  let qcL=0;
+  let qcC=0;
+  for (let line of lines) {
+    if (line>20) qcL++;
+  }
+  for (let col of cols) {
+    if (col>20) qcC++;
+  }
+  qc=qcL*qcC;
+  return qc;
+}
+
+moveRobots(reps);
 
 let qc=[0,0,0,0];
 
-let qc2=0;
 
 for (const r of robots) {
   if      (r.pN[0]<(width-1)/2 && r.pN[1]<(height-1)/2) qc[0]++;
@@ -58,23 +77,31 @@ for (const r of robots) {
 
 
 let safety_factor = qc[0] * qc[1] * qc[2] * qc[3];
-console.log(qc, safety_factor, qc2);
+console.log(qc, safety_factor);
 
-// render
+// part 2
 //
-//
-if (qc2==0) 
-for (let y=0; y<height; y++) {
-  let line="";
-  for (let x=0; x<width; x++) {
-    let cell=0;
-    for (const r of robots) {
-      if (r.pN[0]==x && r.pN[1]==y) {
-        cell++;
-      }
-    }
-    if (cell==0) line=line+".";
-    else line=line+cell;
+for (let reps2=0; reps2<20000; reps2++) {
+  let qc2=moveRobots(reps2);
+  if (qc2>=2) {
+    console.log(reps2);
+    // render
+    //
+    //
+    for (let y=0; y<height; y++) {
+      let line="";
+      for (let x=0; x<width; x++) {
+        let cell=0;
+        for (const r of robots) {
+          if (r.pN[0]==x && r.pN[1]==y) {
+            cell++;
+          }
+        }
+        if (cell==0) line=line+".";
+        else line=line+cell;
+      };
+      console.log(line);
+    };
   };
-  console.log(line);
 };
+
