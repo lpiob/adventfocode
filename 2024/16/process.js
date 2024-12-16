3f12529d9d89c2a812ad60030c93d67ee5bb527a
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-16 08:37:45 +0100

    Discard paths that have higher score that current best known

--- 2024/16/process.js
+++ 2024/16/process.js
@@ -4,6 +4,9 @@ const width=input.split("\n")[0].length;
 const height=input.match(/^#/gms).length;
 console.log(width, height);
 
+var scorecache={};
+let lowest_known_score;
+
 const map=[];
 
 let Sp=[]; // y, x, dir
@@ -43,15 +46,23 @@ console.log(Sp, Ep);
 
 visited=new Set();
 let paths=go(map, Sp, 'S', visited);
-console.log(paths);
 
 let i=0;
 for (const path of paths) {
   i++;
+  let score=calculatePathScore(path);
+  if (score==lowest_known_score)
+  console.log('path',i,'score',score,path);
+}
+
+
+function calculatePathScore(path) {
+  if (scorecache[path]) return scorecache[path];
   let score=0;
   for (const c of path) 
     score+=movePrices[c] || 0
-  console.log('path',i,'score',score,path);
+  scorecache[path]=score;
+  return score;
 }
 
 function drawMap(map) {
@@ -64,9 +75,10 @@ function drawMap(map) {
 
 function go(map, Sp, path='', visited) {
   visited.add(`${Sp[0]}x${Sp[1]}`);
+  let current_score=calculatePathScore(path);
 
   let paths=[];
-  console.log('We are at',Sp[0],Sp[1], 'and we are facing', Sp[2]);
+  //console.log('We are at',Sp[0],Sp[1], 'and we are facing', Sp[2]);
   // where can we go?
   let lr=Sp[2]; // current rotation
 
@@ -76,28 +88,35 @@ function go(map, Sp, path='', visited) {
     paths=[...paths, ...go(map, [ny, nx, Sp[2]], path+'f', new Set(visited))];
   else if (map[ny][nx]=='E') {// going forward would end in finish!
     paths.push(path+'fE');
+    console.log(path+'fE');
+    let score=calculatePathScore(path+'fE');
+    if (!lowest_known_score || score<lowest_known_score) {
+      lowest_known_score=score;
+      console.log("New lowest known score", lowest_known_score);
+    };
     return paths;
   }
-  // rotate
-  lr--;
-  if (lr<0) lr=3; // hardcoded
-  [ny,nx]=[Sp[0]+directions[lr][0], Sp[1]+directions[lr][1]  ];
-  if (!visited.has(`${ny}x${nx}`) && map[ny][nx]=='.') {
-    paths=[...paths, ...go(map, [ny, nx, lr], path+'Cf', new Set(visited))];
-  }
-  // rotate
-  lr--;
-  if (lr<0) lr=3; // hardcoded
-  lr--;
-  if (lr<0) lr=3; // hardcoded
-  [ny,nx]=[Sp[0]+directions[lr][0], Sp[1]+directions[lr][1]  ];
-  if (!visited.has(`${ny}x${nx}`) && map[ny][nx]=='.')
-    paths=[...paths, ...go(map, [ny, nx, lr], path+'Rf', new Set(visited))];
+
+  if (!lowest_known_score || current_score+1000<lowest_known_score) {
+    // rotate counterclockwise
+    lr--;
+    if (lr<0) lr=3; // hardcoded
+    [ny,nx]=[Sp[0]+directions[lr][0], Sp[1]+directions[lr][1]  ];
+    if (!visited.has(`${ny}x${nx}`) && map[ny][nx]=='.') {
+      paths=[...paths, ...go(map, [ny, nx, lr], path+'Cf', new Set(visited))];
+    }
+
+    // rotate clockwise
+    lr--;
+    if (lr<0) lr=3; // hardcoded
+    lr--;
+    if (lr<0) lr=3; // hardcoded
+    [ny,nx]=[Sp[0]+directions[lr][0], Sp[1]+directions[lr][1]  ];
+    if (!visited.has(`${ny}x${nx}`) && map[ny][nx]=='.')
+      paths=[...paths, ...go(map, [ny, nx, lr], path+'Rf', new Set(visited))];
+  };
 
   //@TODO situation when rotating would result in meeting E
-  
-  
-
   //paths.push(path); // @todo limit to those ending on E
   return paths;
 }
