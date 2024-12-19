f44d1932a952958d4598df6e61244ca344f57a48
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-19 11:40:55 +0100

    Optimizations until limits of nodejs memory

--- 2024/19/process.js
+++ 2024/19/process.js
@@ -1,6 +1,87 @@
 const input = require('fs').readFileSync(0).toString().trim(); 
 
-let pqValues = [];
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(element, priority) {
+    this.values.push({ element, priority });
+    this._bubbleUp();
+  }
+
+  dequeue() {
+    if (this.values.length === 0) return null;
+    
+    const min = this.values[0];
+    const end = this.values.pop();
+    
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this._sinkDown();
+    }
+    
+    return min;
+  }
+
+  _bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      
+      if (element.priority >= parent.priority) break;
+      
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  _sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+      
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) || 
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      
+      if (swap === null) break;
+      
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+
+  get length() {
+    return this.values.length;
+  }
+}
+
+let pq = new PriorityQueue();
 let pqVisited = new Set();
 
 const lines = input.split("\n");
@@ -36,18 +117,34 @@ for (const design of desired_designs)
 console.log('possible designs:', sum);
 console.log('possible step2 designs:', sum2);
 
+function buildPatternIndex(design, patterns) {
+  const positionPatterns = new Array(design.length).fill(null).map(() => []);
+  
+  for (const pattern of patterns) {
+    for (let pos = 0; pos <= design.length - pattern.length; pos++) {
+      if (design.substring(pos, pos + pattern.length) === pattern) {
+        positionPatterns[pos].push(pattern);
+      }
+    }
+  }
+  
+  return positionPatterns;
+}
+
 function pqsolve(design, patterns) {
-  pqValues=[];
+  pq=new PriorityQueue();
   pqVisited.clear();
   // State: [cost, current_pattern, current_comma_path]
   const startState = [0, '', ''];
-  pqEnqueue(startState, 0);
+  pq.enqueue(startState, 0);
   pqVisited.add('');
   let bestPaths = [];
 
+  let patternIndex=buildPatternIndex(design, patterns);
+  console.log(patternIndex);
 
-  while (pqValues.length !== 0) {
-    const {element: [cost, cpath, ccpath]} = pqDequeue();
+  while (pq.length !== 0) {
+    const {element: [cost, cpath, ccpath]} = pq.dequeue();
     //console.log('trying',ccpath);
 
     if (cpath==design) {
@@ -59,28 +156,23 @@ function pqsolve(design, patterns) {
       continue;
     }
 
-    for (const p of patterns) {
+    //console.log('cpath',cpath);
+    //console.log('cpath.length-1=', cpath.length);
+    //console.log('pi[^]=', patternIndex [ cpath.length ]);
+    //for (const p of patterns) {
+    for (const p of patternIndex[cpath.length]) {
       //const ncpath=ccpath+(ccpath.length>0?',':'')+p;
       const ncpath=ccpath+','+p;
       const npath=cpath+p;
       if (pqVisited.has(ncpath)) continue;
       if (npath.length>design.length) continue;
       if (design.startsWith(npath)) {
-        pqEnqueue([cost+1, npath, ncpath], cost+1);
+        pq.enqueue([cost+1, npath, ncpath], cost+1);
         pqVisited.add(ncpath);
       }
     };
 
   }
   return bestPaths;
-}
-
-function pqEnqueue(element, priority) {
-    pqValues.push({element, priority});
-    pqValues.sort((a, b) => a.priority - b.priority);
-}
-
-function pqDequeue() {
-    return pqValues.shift();
 }
 
