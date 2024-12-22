40ec4b8e6be0ed4ef6a30c1d8d990473f53a3f88
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-22 21:41:05 +0100

    A solution thats cpu-bound instead of being memory bound

--- 2024/21/process_step2.js
+++ 2024/21/process_step2.js
@@ -14,6 +14,8 @@ const dirpad=[
   [..."<v>"]
 ];
 
+let dirpadpositions=[];
+
 let keypad_distance={};
 for (let y1=0; y1<4; y1++) {
   for (let x1=0; x1<3; x1++) {
@@ -45,6 +47,7 @@ for (let y1=0; y1<4; y1++) {
 keypad_distance["A-1"].keys="^<<";
 keypad_distance["A-4"].keys="^^<<";
 keypad_distance["A-7"].keys="^^^<<";
+keypad_distance["2-9"].keys=">^^";
 
 let dirpad_distance={};
 for (let y1=0; y1<2; y1++) {
@@ -73,7 +76,37 @@ for (let y1=0; y1<2; y1++) {
   };
 };
 
-let sum=0;
+dirpad_distance['<-^'].keys=">^";
+dirpad_distance['<-A'].keys=">>^";
+dirpad_distance['<-v'].keys=">";
+dirpad_distance['<->'].keys=">>";
+
+dirpad_distance['v-<'].keys="<";
+dirpad_distance['v->'].keys=">";
+dirpad_distance['v-A'].keys=">^";
+dirpad_distance['v-v'].keys="";
+
+dirpad_distance['A-v'].keys="<v";
+dirpad_distance['A-^'].keys="<";
+dirpad_distance['A-<'].keys="v<<";
+dirpad_distance['A->'].keys="v";
+
+dirpad_distance['>-A'].keys="^";
+dirpad_distance['>-^'].keys="<^";
+dirpad_distance['>-v'].keys="<";
+dirpad_distance['>-<'].keys="<<";
+
+dirpad_distance['^->'].keys=">v";
+dirpad_distance['^-A'].keys=">";
+dirpad_distance['^-<'].keys="v<";
+dirpad_distance['^-v'].keys="v";
+
+//console.log(getDirpadCost('A', '<', 25));
+//process.exit(0);
+
+let sum2=0;
+let sum25=0;
+let finalkeyslen=0;
 
 for (const code of codes) {
   console.log('About to type',code);
@@ -102,35 +135,69 @@ for (const code of codes) {
   console.log('keystack', keystack);
   console.log('cost of keypad presses:', keypad_cost);
 
-  // wiemy ile ruchów potrzeba aby się przesunąc na nowy klawisz
-  // wiemy ile razy kazdy klawisz trzeba nacisnąć
-  // czy mozemy to przetlumaczyc na ilosc nacisniec dirpad?
-  // sprawdzamy po kolei
-  let dirstack={};
+  for (let i=0; i<25;i++) {
+    dirpadpositions[i]='A';
+  };
 
   // convert keystack to dirstack
+  let sum_cost=0;
+  const n=2;
   for (const c of keystack) {
-    dirstack[c] = (dirstack[c]||0) + 1;
+    console.log(c);
+    let cost=getDirpadCost(dirpadpositions[n], c, n);
+    sum_cost+=cost
   };
-  console.log(dirstack);
-
-  let dirpad_cost=0;
-  [dirstack, dirpad_cost]=getDirpadKeys(dirstack);
-  console.log('dirstack', dirstack);
-/*
-  [dirstack, dirpad_cost]=getDirpadKeys(dirstack);
-  console.log('dirstack', dirstack);
-  */
-
   let code_number=Number(code.replace(/^0*(\d+)A$/,'$1'));
-  for (let k in dirstack) {
-    sum+=dirstack[k]*code_number;
-  };
-  //sum+=dirstack.length*code_number;
+  console.log(code,'=',sum_cost,'*',code_number);
+  sum2+=sum_cost*code_number;
 };
 
-console.log('sum', sum);
-console.log(keypad_distance["A-4"])
+console.log('sum2', sum2);
+
+let cache=new Map();
+
+function getDirpadCost(from,to,n) {
+  let cost=0;
+  let keys='';
+  let k=dirpad_distance[`${from}-${to}`];
+  console.log(`${from}-${to}-${n}`);
+  //
+
+  if (k.keys && n>0) {
+    keys+=k.keys;
+    for (const c of k.keys) {
+      //console.log("subcheck",c,n-1);
+      const subcost=getDirpadCost(dirpadpositions[n-1], c, n-1);
+      cost+=subcost;
+    };
+  } else if (from==to && n>0) {
+    //keys+='A';
+    //const subcost=getDirpadCost(dirpadpositions[n], 'A', n-1);
+    //cost+=subcost;
+  };
+
+  if (n>0) {
+    keys += 'A';
+    const subcost=getDirpadCost(dirpadpositions[n-1], 'A', n-1);
+    cost+=subcost;
+  };
+  
+
+  //if (n==0)
+  //console.log("k",n,keys);
+  
+  if (n==0) {
+    //console.log('n0 keys to press', to, '=', keys);
+    //console.log('local cost', k.dist+1);
+    cost+=k.dist;
+    dirpadpositions[n]=to;
+    return cost;
+  };
+  dirpadpositions[n]=to;
+  cost+=keys.length;
+  return cost;
+
+};
 
 function getDirpadKeys(keystack) {
   console.log('analyzing for',keystack);
