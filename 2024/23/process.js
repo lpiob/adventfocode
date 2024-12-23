5b9737ca00be8aee90b2b9e3bcb07fa24fdeb247
Lukasz Biegaj <lukasz.biegaj@unity.pl>
2024-12-23 08:11:50 +0100

    Part2

--- 2024/23/process.js
+++ 2024/23/process.js
@@ -1,6 +1,7 @@
 const input = require('fs').readFileSync(0).toString().trim(); 
 
 let networksof3=new Set();
+let networks=[];
 let p2p_links=new Map();
 
 for (const line of input.split("\n")) {
@@ -14,6 +15,23 @@ for (const line of input.split("\n")) {
   el=p2p_links.get(h2) || new Set();
   el.add(h1);
   p2p_links.set(h2, el);
+/*
+  // networks
+  let n1=findNetworksWithHost(networks,h1);
+  let n2=findNetworksWithHost(networks,h2);
+  if (n1.length<=0 && n2.length<=0) {
+    networks.push(new Set([h1, h2]));
+  };
+
+  //console.log(n1,n2);
+
+  for (const n of n1) {
+    n.add(h2);
+  };
+  for (const n of n2) {
+    n.add(h1);
+  };
+  */
 }
 
 for (let [h1, links] of p2p_links) {
@@ -33,8 +51,16 @@ for (let [h1, links] of p2p_links) {
 
 
 
+
+
 console.log(p2p_links);
-console.log(networksof3);
+const cliques = BronKerbosch1(new Set(), new Set(p2p_links.keys()), new Set());
+let maxc=new Set();
+for (const c of cliques) {
+  if (c.size>maxc.size) maxc=c;
+};
+
+const password=[...maxc.values()].sort().join(',');
 
 let sum1=0;
 for (const el of networksof3) {
@@ -44,4 +70,42 @@ for (const el of networksof3) {
 };
 
 console.log("Count of networks of 3 containing t-computers:", sum1);
+console.log("Password:", password);
+
+
+// https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+// algorithm BronKerbosch1(R, P, X) is
+function BronKerbosch1(R, P, X) {
+  let cliques = [];
+    
+  //if P and X are both empty then
+  //  report R as a maximal clique
+  if (P.size <= 0 && X.size <= 0)
+    return [new Set(R)];
+    
+  //for each vertex v in P do
+  for (const v of Array.from(P)) {
+    // BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+    const neighbors = p2p_links.get(v)||new Set();
+    R.add(v);
+
+    cliques = cliques.concat(
+      BronKerbosch1(
+        R,
+        intersection(P, neighbors),
+        intersection(X, neighbors),
+        p2p_links
+      )
+    );
+    R.delete(v);
+    P.delete(v);
+    X.add(v);
+  }
+  
+  return cliques;
+}
+
+function intersection(setA, setB) {
+    return new Set([...setA].filter(x => setB.has(x)));
+}
 
