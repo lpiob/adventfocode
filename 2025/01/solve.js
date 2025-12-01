256c0eefa30d494df4a25b87090aa483301b2d62
Lukasz Biegaj <ghlb@x93.org>
2025-12-01 08:22:10 +0100

    intermediate p2, alas not working yet

--- 2025/01/solve.js
+++ 2025/01/solve.js
@@ -10,13 +10,24 @@ console.log('The dial starts by pointing at', pos);
 for (const line of lines) {
   let [direction, distance]=line.match(/^([A-Z])(\d+)$/).slice(1);
   if (direction==='L') distance=-distance;
+  const original_pos=pos;
+  console.log('Original position:', original_pos, 'original reached_zero:', reached_zero);
   pos+=Number(distance);
   //if (pos<0) pos+=100;
   //if (pos>=100) pos-=100;
-  while (pos>=100) pos-=100;
-  while (pos<0) pos+=100;
-  console.log('The dial is rotated', direction, distance, 'to point at', pos, pos===0?' (reached zero!)':'');
-  if (pos===0) reached_zero+=1;
+  let passed_zero = 0;
+  while (pos>=100) { 
+    pos-=100; 
+    if (pos!==0) passed_zero++;
+  }
+  while (pos<0) { 
+    pos+=100; 
+    if (original_pos!=0) passed_zero++;
+  }
+  console.log('The dial is rotated', direction, distance, 'to point at', pos, pos===0?' (reached zero!)':'', (passed_zero>0?`during this rotation, it points at 0x${passed_zero} timess`:''));
+  reached_zero+=passed_zero;
+  if (pos===0) reached_zero++;
+  console.log('Current reached_zero:', reached_zero);
 };
 
 console.log('password', reached_zero);
