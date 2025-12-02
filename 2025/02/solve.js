23e21d25d21531abafda02bedd66e129cb20807b
Lukasz Biegaj <ghlb@x93.org>
2025-12-02 06:43:55 +0100

    Part 2
    well, to answer the question in previous commit: yes

--- 2025/02/solve.js
+++ 2025/02/solve.js
@@ -2,17 +2,21 @@ const input = require('fs').readFileSync(0).toString().trim();
 const ranges=input.split(",");
 
 let p1=0;
+let p2=0;
 
 for (const range of ranges) {
   console.log('range', range);
   let [start, end]=range.split("-").map(Number);
   for (let i=start; i<=end; i++) {
     if (String(i).match(/^(\d+)\1$/)) {
-      console.log(i);
       p1=p1+i;
+    }
+    if (String(i).match(/^(\d+)\1+$/)) {
+      console.log(i);
+      p2=p2+i;
     }
   }
 }
 
 console.log("Part 1:", p1);
-
+console.log("Part 2:", p2);
