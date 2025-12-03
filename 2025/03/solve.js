02369c1da538217b2dca7d653f7b99475eb6df36
Lukasz Biegaj <ghlb@x93.org>
2025-12-03 09:58:02 +0100

    working p2
    this took a while but was a pleasant exercise
    first p2 attempt was rather brute-forcish but allowed me to observe patterns
    within the algorithm
    second p2 attempt is utilizing this attempts to deliver near-instant
    results

--- 2025/03/solve.js
+++ 2025/03/solve.js
@@ -4,34 +4,39 @@ const banks=input.split("\n");
 let p1=0;
 let p2=0;
 
-function findNextJolt(numbers, len) {
-  // console.log("findNextJolt called with:", numbers, len);
-  let pairs=[];
-  for (let i=0; i<numbers.length-len; i++) {
-    const current_digit=numbers[i];
-    if (len==0) { 
-      return current_digit;
-    }
-    for (let j=i+1; j<numbers.length-len+1; j++) {
-        // console.log('current len:', len, 'current digit:', current_digit, 'looking at:', numbers[j]);
-        // console.log('recursive call with:', [...numbers].splice(j), len-1);
-        const next_number=findNextJolt([...numbers].splice(j), len-1);
-        pairs.push(Number(String(current_digit)+String(next_number)));
-    }
-  };
-  // console.log('all pairs found:', pairs);
-  pairs.sort((a,b)=>b-a);
-  // console.log('returning pairs:', pairs[0]);
-  return pairs[0];
-};
+function findFirstLargestDigitInSet(numbers) {
+  const val = [...numbers].sort((a,b)=>b-a)[0];
+  const idx = numbers.indexOf(val);
+  
+  return {val, idx}
+}
+
+function findMaxJolt(numbers, len) {
+  console.log('findMaxJolt called with:', numbers, len);
+  // start by finding the largest digit that will start the jolt
+  console.log('finding largest digit in set:', numbers.slice(0, numbers.length - len + 1));
+  const {val, idx} = findFirstLargestDigitInSet(numbers.slice(0, numbers.length - len + 1));
+  // continue by finding the next largest digit in the remaining set
+  if (len > 1) {
+    console.log('finding next jolt in remaining set:', numbers.slice(idx + 1));
+    const next_jolt = findMaxJolt(numbers.slice(idx + 1), len - 1);
+    console.log('next jolt found:', next_jolt);
+    return Number(String(val) + String(next_jolt));
+  }
+  return val;
+}
 
 for (const bank of banks) {
   let numbers = bank.split('').map(Number);
   console.log('bank:', bank, numbers);
-  const smallJolt=findNextJolt(numbers, 1);
-  const largeJolt=findNextJolt(numbers, 11);
-  console.log('small jolt:', smallJolt);
-  console.log('large jolt:', largeJolt);
+  const smallJolt=findMaxJolt(numbers, 2);
+  const largeJolt=findMaxJolt(numbers, 12);
+  //
+  //
+  console.log('largest digit in set:', findFirstLargestDigitInSet(numbers));
+  console.log('max jolt(2) found:', smallJolt);
+  console.log('max jolt(12) found:', largeJolt);
+
 
   p1+=smallJolt;
   p2+=largeJolt;
