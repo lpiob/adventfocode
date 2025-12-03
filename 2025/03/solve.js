d47886f92df4f40c201e1447c70dd13d87ba7f52
Lukasz Biegaj <ghlb@x93.org>
2025-12-03 08:02:50 +0100

    working part1

--- 2025/03/solve.js
+++ 2025/03/solve.js
@@ -7,15 +7,15 @@ let p2=0;
 for (const bank of banks) {
   let numbers = bank.split('').map(Number);
   console.log('bank:', bank, numbers);
-  const indices = [...numbers]
-    .map((val, idx) => ({val, idx}))
-    .sort((a, b) => b.val - a.val)
-    .slice(0, 2)
-    .map(item => item.idx)
-    .sort((a, b) => a - b);
-  const largest_pair = Number(indices.map(i => numbers[i]).join(''));
-  console.log('bank:', bank, largest_pair, indices);
-  p1 += largest_pair;
+  let pairs = [];
+  for (let i=0; i<numbers.length-1; i++) {
+    const current_digit=numbers[i];
+    const next_max_digit=numbers.slice(i+1).sort((a,b)=>b-a)[0]||-1;
+    pairs.push(current_digit*10+next_max_digit);
+  }
+  pairs.sort((a,b)=>b-a);
+  console.log('greatest pair:', pairs[0]);
+  p1+=pairs[0];
 }
 
 console.log("Part 1:", p1);
