d9626e4f8c46e1ccb2fd5aa57de4a5df908a8a5d
Lukasz Biegaj <ghlb@x93.org>
2025-12-08 08:30:40 +0100

    p1 and a new structure learned

--- 2025/08/solve.js
+++ 2025/08/solve.js
@@ -3,9 +3,10 @@ let p1=0;
 let p2=0;
 
 let jbs=[];
+let i=0;
 for (const line of input.split('\n')) {
   const pos=line.split(",").map(Number);
-  jbs.push({ pos: pos, connections: [] });
+  jbs.push({ pos: pos, idx: i++ });
 }
 
 function distance(jb1, jb2) {
@@ -26,9 +27,42 @@ for (let i=0; i<jbs.length; i++) {
 pairs.sort((a,b) => a.dist - b.dist);
 
 console.log("All pairs sorted by distance:");
-for (const pair of pairs) {
-  console.log(`Distance: ${pair.dist.toFixed(2)}, JB1: ${pair.jb1.pos}, JB2: ${pair.jb2.pos}`);
+
+let dfu_parent = Array(jbs.length).fill(0).map((_, idx) => idx);
+let dfu_size = Array(jbs.length).fill(1);
+
+console.log('parents', dfu_parent);
+console.log('sizes', dfu_size);
+
+function dfu_find(x) {
+  if (dfu_parent[x] !== x)
+    dfu_parent[x] = dfu_find(dfu_parent[x]);
+
+  return dfu_parent[x];
 }
+
+for (let i=0; i<1000; i++) {
+  const pair=pairs.shift();
+  if (!pair) break;
+//  console.log(`Processing pair with distance ${pair.dist.toFixed(2)} between JB1: ${pair.jb1.pos} and JB2: ${pair.jb2.pos}`);
+  //
+  const root1 = dfu_find(pair.jb1.idx);
+  const root2 = dfu_find(pair.jb2.idx);
+
+  if (root1 === root2) continue;
+
+  dfu_parent[root2] = root1;
+  dfu_size[root1] += dfu_size[root2];  
+};
+console.log('parents', dfu_parent);
+console.log('sizes', dfu_size);
+
+// top 3 sizes
+const rootSizes = dfu_size.filter((size, idx) => dfu_parent[idx] === idx);
+console.log('Top 3 sizes:', rootSizes.sort((a,b) => b - a).slice(0,3));
+
+// multiply the above
+p1 = rootSizes.sort((a,b) => b - a).slice(0,3).reduce((a,b) => a*b, 1);
 
 console.log("Part 1:", p1);
 console.log("Part 2:", p2);
